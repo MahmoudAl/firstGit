d2300ebc82c33a2d3738e70aa8d1d99402046f97
mahmoudalio <mahmoud.alio@extern.intenta.de>
2024-08-06 23:43:03 -0700

    adding calcAvg function

---
+++ calcAvg.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+int calcAvg(std::vector <int> v) {
+    int n = v.size();
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += v[i];
+    }
+    return (double)sum / n;
+}
